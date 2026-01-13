018147b5055363bc811faf5ee09fcb4e7105b60b
Sky <sky@leite.dev>
2026-01-13 15:32:57 -0300

    Add flake.nix to redrectangle example

--- examples/vitasdk/redrectangle/flake.nix
+++ examples/vitasdk/redrectangle/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    hbnix.url = "github:SkyLeite/hbnix";
+    hbnix.url = "github:SkyLeite/hbnix?ref=main";
   };
 
   outputs =
@@ -13,17 +13,34 @@
       hbnix,
     }:
     let
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ hbnix.overlays."${system}" ];
+        };
+
       forAllSystems =
         function:
         nixpkgs.lib.genAttrs [
           "x86_64-linux"
           "aarch64-linux"
           "aarch64-darwin"
-        ] (system: function nixpkgs.legacyPackages.${system} system);
+        ] (system: function (mkPkgs system) system);
 
-      hbn = forAllSystems (pkgs: system: pkgs.callPackage hbnix."${system}");
+      hbn = forAllSystems (pkgs: system: hbnix.lib."${system}");
     in
     {
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      packages = forAllSystems (
+        pkgs: system: {
+          default = (
+            hbn.mkVitaPackage {
+              name = "redrectangle";
+              src = ./.;
+              buildInputs = [ pkgs.vitaPackages.sdl2 ];
+            }
+          );
+        }
+      );
     };
 }
